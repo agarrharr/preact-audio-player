cb391845e1009159a0e332ee00201c3c397d98c8
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2017-09-04 13:38:51 -0600

    Add times and prevent dragging from highlighting text

--- src/components/widget.js
+++ src/components/widget.js
@@ -1,25 +1,14 @@
 import { h, Component } from 'preact';
+import leftPad from 'left-pad';
 
-const VOLUME_WIDTH = 20;
+const VOLUME_WIDTH = 100;
 const BLUE = '#3FB3D2';
 const BLUE_DARK = '#1A83A1';
+const BLUE_2 = '#00A0AD';
+const WHITE = '#FFFFFF';
 
-const styles = {
-  PreactAudioPlayer__VolumeSlider: {
-    backgroundColor: 'red',
-    height: 20,
-    width: VOLUME_WIDTH,
-  },
-  PreactAudioPlayer__Play: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: BLUE,
-    width: 100,
-    height: 100,
-    borderRadius: '50%',
-    border: `5px solid ${BLUE_DARK}`,
-  },
+const getMinutesAndSeconds = time => {
+  return `${leftPad(Math.floor(time / 60), 2, '0')}:${leftPad(time % 60, 2, '0')}`
 };
 
 const PlayIcon = () =>
@@ -37,6 +26,8 @@ export default class Widget extends Component {
     isMuted: false,
     isPlaying: false,
     volume: 1,
+    currentTime: null,
+    duration: null,
   };
 
   static defaultProps = {
@@ -47,6 +38,13 @@ export default class Widget extends Component {
     document.addEventListener('mousemove', this.handleMouseMove.bind(this));
     document.addEventListener('mouseup', this.handleMouseUp.bind(this));
     this.audio = document.getElementById('PreactAudioPlayer');
+
+    // this.interval = setInterval(() => this.setState({ currentTime: this.audio.currentTime }), 1000);
+    // this.setState({ duration: this.audio.duration });
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
   }
 
   handlePlayClick = () => {
@@ -54,6 +52,12 @@ export default class Widget extends Component {
 
     if (this.state.isPlaying) {
       this.audio.play();
+      this.audio.addEventListener('timeupdate', (event) => {
+        this.setState({
+          currentTime: Math.floor(this.audio.currentTime),
+          duration: Math.floor(this.audio.duration),
+        });
+      }, false);
     } else {
       this.audio.pause();
     }
@@ -74,6 +78,7 @@ export default class Widget extends Component {
 
   handleMouseMove = (e) => {
     if (this.state.isVolumeChanging) {
+      console.log(this.state.clientWidth);
       const relativePosition = (e.x - this.state.clientX) / this.state.clientWidth;
       const volume = relativePosition < 0
           ? 0
@@ -92,6 +97,7 @@ export default class Widget extends Component {
   };
 
   handleVolumeMouseDown = (e) => {
+    e.preventDefault();
     this.setState({
       isVolumeChanging: true,
       clientX: e.x - e.offsetX,
@@ -105,22 +111,83 @@ export default class Widget extends Component {
 
   render() {
     const {url} = this.props;
-    const {isMuted, isPlaying, volume} = this.state;
+    const {isMuted, isPlaying, volume, currentTime, duration} = this.state;
+    const styles = {
+      PreactAudioPlayer: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        backgroundColor: BLUE_DARK,
+        height: 50,
+      },
+      PreactAudioPlayer__Play: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: 100,
+        height: 100,
+        marginLeft: 20,
+        backgroundColor: BLUE,
+        borderRadius: '50%',
+        border: `5px solid ${BLUE_DARK}`,
+      },
+      PreactAudioPlayer__Time: {
+        color: WHITE,
+      },
+      PreactAudioPlayer__Volume: {
+        display: 'flex',
+      },
+      PreactAudioPlayer__Mute: {
+        color: WHITE,
+        marginRight: 20,
+      },
+      PreactAudioPlayer__VolumeSlider: {
+        position: 'relative',
+        height: 20,
+        width: VOLUME_WIDTH,
+        marginRight: 20,
+        backgroundColor: WHITE,
+        borderRadius: 15,
+      },
+      PreactAudioPlayer__VolumeSliderHandle: {
+        position: 'relative',
+        height: 20,
+        width: 20,
+        top: -20,
+        left: isMuted ? 0 : (volume * (VOLUME_WIDTH - 20)),
+        backgroundColor: BLUE,
+        borderRadius: '50%',
+        pointerEvents: 'none',
+      },
+      PreactAudioPlayer__VolumeSliderFill: {
+        height: 20,
+        width: isMuted ? 0 : (volume * (VOLUME_WIDTH - 20)) + 20,
+        backgroundColor: BLUE_2,
+        borderRadius: 15,
+        pointerEvents: 'none',
+      },
+    };
+
 
     return (
-      <div>
-        <audio id="PreactAudioPlayer" width="300" height="32" controls="controls">
+      <div style={styles.PreactAudioPlayer}>
+        <audio id="PreactAudioPlayer">
           <source src={url} type="audio/mp3" />
         </audio>
         <button type="button" title="Play/Pause" aria-label="Play/Pause" style={styles.PreactAudioPlayer__Play} onClick={this.handlePlayClick}>
           {isPlaying ? <PauseIcon /> : <PlayIcon />}
         </button>
-        <div class="PreactAudioPlayer__Volume">
-	  {isMuted ? 'Muted' : volume}
-          <div class="PreactAudioPlayer__Mute" onClick={this.handleMuteClick}>
+        <div style={styles.PreactAudioPlayer__Time}>
+          {getMinutesAndSeconds(currentTime)}/{getMinutesAndSeconds(duration)}
+        </div>
+        <div style={styles.PreactAudioPlayer__Volume} >
+          <div style={styles.PreactAudioPlayer__Mute} onClick={this.handleMuteClick}>
             {isMuted ? 'Unmute' : 'Mute'}
           </div>
-          <div style={styles.PreactAudioPlayer__VolumeSlider} onMouseMove={this.handleVolumeMouseMove} onMouseDown={this.handleVolumeMouseDown}></div>
+          <div style={styles.PreactAudioPlayer__VolumeSlider} onMouseMove={this.handleVolumeMouseMove} onMouseDown={this.handleVolumeMouseDown}>
+            <div style={styles.PreactAudioPlayer__VolumeSliderFill}></div>
+            <div style={styles.PreactAudioPlayer__VolumeSliderHandle}></div>
+          </div>
         </div>
       </div>
     );
